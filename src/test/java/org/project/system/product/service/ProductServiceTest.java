a60b5855591acd8d11476dc2eaafa84cb6bdc833
Teodor Coman <coman.teodor@yaho.com>
2019-04-08 10:56:37 +0300

    Corrected unit test

--- src/test/java/org/project/system/product/service/ProductServiceTest.java
+++ src/test/java/org/project/system/product/service/ProductServiceTest.java
@@ -7,18 +7,22 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.project.system.product.ProductException;
 import org.project.system.product.domain.attribute.Attribute;
 import org.project.system.product.domain.product.PredefinedStructure;
 import org.project.system.product.domain.product.PredefinedStructureAttribute;
-import org.project.system.product.domain.product.Product;
-import org.project.system.product.domain.product.ProductAttribute;
 import org.project.system.product.repository.ProductRepository;
+import org.project.system.product.service.model.SaveProductRequest;
 import org.project.system.productsearch.service.ProductSearchService;
+import org.project.system.user.domain.Account;
+import org.project.system.user.domain.User;
+import org.project.system.user.service.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductServiceTest {
@@ -31,6 +35,12 @@ public class ProductServiceTest {
     @Mock
     private ProductSearchService productSearchService;
 
+    @Mock
+    private PredefinedStructureService predefinedStructureService;
+
+    @Mock
+    private UserService userService;
+
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
@@ -40,64 +50,138 @@ public class ProductServiceTest {
     public void setUp(){
         productService.setProductSearchService(productSearchService);
         productService.setProductRepository(productRepository);
+        productService.setPredefinedStructureService(predefinedStructureService);
+        productService.setUserService(userService);
     }
 
 
     @Test
     public void test_CreateProductWithDifferentAttributesThanPredefinedStructure() throws ProductException{
-        Product product = new Product();
 
-        PredefinedStructure predefinedStructure = new PredefinedStructure();
+        SaveProductRequest saveProductRequest = new SaveProductRequest();
+        saveProductRequest.setPredefinedStructureId(1l);
+        saveProductRequest.setUserId(2L);
+
+        List<org.project.system.product.service.model.Attribute> attributes = new ArrayList<>();
+        org.project.system.product.service.model.Attribute attribute = new org.project.system.product.service.model.Attribute();
+        attribute.setId(2L);
+        attributes.add(attribute);
+        saveProductRequest.setAttributes(attributes);
+
+        PredefinedStructure predefinedStructure = createPredefinedStructureWithUser();
         List<PredefinedStructureAttribute> structureAttributes = new ArrayList();
         PredefinedStructureAttribute predefinedStructureAttribute = new PredefinedStructureAttribute();
         Attribute structureAttribute = new Attribute();
         structureAttribute.setId(1L);
+
         predefinedStructureAttribute.setAttribute(structureAttribute);
         predefinedStructure.setStructureAttributes(structureAttributes);
         structureAttributes.add(predefinedStructureAttribute);
-        product.setPredefinedStructure(predefinedStructure);
 
-        List<ProductAttribute> productAttributes = new ArrayList<>();
-        ProductAttribute productAttribute = new ProductAttribute();
-        Attribute attribute = new Attribute();
-        attribute.setId(2L);
-        productAttribute.setAttribute(attribute);
-        productAttributes.add(productAttribute);
-        product.setProductAttributes(productAttributes);
+        Mockito.when(predefinedStructureService.find(1l)).thenReturn(Optional.of(predefinedStructure));
+
+        User requestUser = new User();
+        requestUser.setId(2L);
+        Account owner = new Account();
+        owner.setId(3L);
+        requestUser.setOwner(predefinedStructure.getCreatedBy().getOwner());
+        Mockito.when(userService.find(2l)).thenReturn(Optional.of(requestUser));
 
         exception.expect(ProductException.class);
         exception.expectMessage("Product attributes are different than specified in the predefined structure");
 
-        productService.saveProduct(product);
+        productService.saveProduct(saveProductRequest);
     }
 
     @Test
     public void test_CreateProductMissingMandatoryAttribute() throws ProductException{
-        Product product = new Product();
+        SaveProductRequest saveProductRequest = new SaveProductRequest();
+        saveProductRequest.setPredefinedStructureId(1l);
+        saveProductRequest.setUserId(2L);
 
-        PredefinedStructure predefinedStructure = new PredefinedStructure();
+        PredefinedStructure predefinedStructure = createPredefinedStructureWithUser();
         List<PredefinedStructureAttribute> structureAttributes = new ArrayList();
         PredefinedStructureAttribute predefinedStructureAttribute = new PredefinedStructureAttribute();
+        predefinedStructureAttribute.setMandatory(true);
         Attribute structureAttribute = new Attribute();
         structureAttribute.setId(1L);
+
         predefinedStructureAttribute.setAttribute(structureAttribute);
-        predefinedStructureAttribute.setMandatory(true);
+        predefinedStructure.setStructureAttributes(structureAttributes);
         structureAttributes.add(predefinedStructureAttribute);
 
-        predefinedStructure.setStructureAttributes(structureAttributes);
-        product.setPredefinedStructure(predefinedStructure);
+        Mockito.when(predefinedStructureService.find(1l)).thenReturn(Optional.of(predefinedStructure));
+
+        User requestUser = new User();
+        requestUser.setId(2L);
+        Account owner = new Account();
+        owner.setId(3L);
+        requestUser.setOwner(predefinedStructure.getCreatedBy().getOwner());
+        Mockito.when(userService.find(2l)).thenReturn(Optional.of(requestUser));
 
 
         exception.expect(ProductException.class);
         exception.expectMessage("Missing mandatory Attribute");
 
-        productService.saveProduct(product);
+        productService.saveProduct(saveProductRequest);
 
     }
 
     @Test
     public void test_CreateProductWithTemplateOwnedBySomebodyElse() throws ProductException{
+        SaveProductRequest saveProductRequest = new SaveProductRequest();
+        saveProductRequest.setPredefinedStructureId(1l);
+        saveProductRequest.setUserId(2L);
 
+        PredefinedStructure predefinedStructure = createPredefinedStructureWithUser();
+        Mockito.when(predefinedStructureService.find(1l)).thenReturn(Optional.of(predefinedStructure));
+
+        User requestUser = new User();
+        requestUser.setId(2L);
+        Account owner = new Account();
+        owner.setId(3L);
+        requestUser.setOwner(owner);
+        Mockito.when(userService.find(2l)).thenReturn(Optional.of(requestUser));
+
+
+        exception.expect(ProductException.class);
+        exception.expectMessage("PredefinedStructure not owned by User");
+
+        productService.saveProduct(saveProductRequest);
+    }
+
+    @Test
+    public void test_CreateProductWithUnknownPredefinedStructure() throws ProductException{
+        SaveProductRequest saveProductRequest = new SaveProductRequest();
+        saveProductRequest.setPredefinedStructureId(1l);
+        saveProductRequest.setUserId(2L);
+
+        Mockito.when(predefinedStructureService.find(1l)).thenReturn(Optional.empty());
+
+        User requestUser = new User();
+        requestUser.setId(2L);
+        Account owner = new Account();
+        owner.setId(3L);
+        requestUser.setOwner(owner);
+        Mockito.when(userService.find(2l)).thenReturn(Optional.of(requestUser));
+
+
+        exception.expect(ProductException.class);
+        exception.expectMessage("PredefinedStructure doesn't exist");
+
+        productService.saveProduct(saveProductRequest);
+    }
+
+    private PredefinedStructure createPredefinedStructureWithUser(){
+        PredefinedStructure predefinedStructure = new PredefinedStructure();
+        User user = new User();
+        Account account = new Account();
+        account.setId(1L);
+        user.setOwner(account);
+        user.setId(1L);
+        predefinedStructure.setCreatedBy(user);
+
+        return predefinedStructure;
     }
 
 
